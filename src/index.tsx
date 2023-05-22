92cede9630e58b0c7375e954880d6e5c88e03f5a
Hugo Martin <hugo.martin.duloz@gmail.com>
2023-05-22 16:34:37 +0200

    Routes definition

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloProvider } from "@apollo/client";
+import client from "./apollo/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  GET_CONTINENTS_QUERIES,
+  GET_COUNTRIES_FROM_CONTINENT,
+  GET_COUNTRY_FROM_CODE,
+} from "./apollo/queries";
+import ContinentsPage from "./pages/continents";
+import CountryDetail from "./pages/continents/code/countries/code";
+import CountriesPage from "./pages/continents/code/countries";
+import RedirectTo from "./components/RedirectTo";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "continents",
+        element: <ContinentsPage />,
+        loader: async (args) => {
+          console.log(args);
+          const { data } = await client.query({
+            query: GET_CONTINENTS_QUERIES,
+          });
+          return data.continents;
+        },
+      },
+      {
+        path: "continents/:code",
+        element: <RedirectTo to="countries" />,
+      },
+      {
+        path: "continents/:code/countries",
+        element: <CountriesPage />,
+        loader: async (args) => {
+          const { code } = args.params;
+          const { data } = await client.query({
+            query: GET_COUNTRIES_FROM_CONTINENT,
+            variables: { code },
+          });
+          return data.countries;
+        },
+      },
+      {
+        path: "continents/:continentCode/countries/:code",
+        element: <CountryDetail />,
+        loader: async (args) => {
+          const { code } = args.params;
+          const { data } = await client.query({
+            query: GET_COUNTRY_FROM_CODE,
+            variables: { code },
+          });
+          return data.country;
+        },
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
